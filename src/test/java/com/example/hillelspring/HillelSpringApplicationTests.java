ab4a193320beda67ad4be2eed75e566a43d25a52
Andrii Vedmid <medvik9@gmail.com>
2022-06-11 14:54:26 +0300

    Init commit

---
+++ src/test/java/com/example/hillelspring/HillelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hillelspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillelSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

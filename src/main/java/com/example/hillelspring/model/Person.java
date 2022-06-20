186668d3271c75523896ba9de31120647a0c80e0
Andrii Vedmid <medvik9@gmail.com>
2022-06-20 18:21:32 +0300

    lesson25homework

--- src/main/java/com/example/hillelspring/model/Person.java
+++ src/main/java/com/example/hillelspring/model/Person.java
@@ -2,11 +2,13 @@ package com.example.hillelspring.model;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Builder
 @Data
 public class Person {
 
+    @Id
     private Integer id;
     private String name;
 }
--- src/main/java/com/example/hillelspring/service/AnimalService.java
+++ src/main/java/com/example/hillelspring/service/AnimalService.java
@@ -1,57 +1,49 @@
 package com.example.hillelspring.service;
 
 import com.example.hillelspring.model.Animal;
-import com.example.hillelspring.service.lists.AnimalList;
+import com.example.hillelspring.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnimalService {
-
-    private final AnimalList animalList;
-    private final PersonService personService;
+    private final AnimalRepository animalRepository;
 
     @Autowired
-    public AnimalService(AnimalList animalList, PersonService personService) {
-        this.animalList = animalList;
-        this.personService = personService;
+    public AnimalService(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
     }
 
     public Animal getAnimal(Integer id) {
-        Animal animal = animalList.getAnimalList().stream()
-                .filter(s->s.getId().equals(id))
-                .findAny()
+        Animal animal = animalRepository.findById(id)
                 .orElse(Animal.builder().build());
         return animal;
     }
 
     public Animal updateAnimal(Animal restAnimal) {
-        Animal animal = restAnimal;
-
-        var i = animalList.getAnimalList().iterator();
-
-        while(i.hasNext()){
-            animal = i.next();
-            if(animal.getId() == restAnimal.getId()){
-                animal.setNickname(restAnimal.getNickname());
-                break;
-            }
+        Animal animal = findAnimal(restAnimal.getId());
+        if(restAnimal.getNickname() != null){
+        animal.setNickname(restAnimal.getNickname());
         }
-
+        if(restAnimal.getOwner() != null){
+            animal.setOwner(restAnimal.getOwner());
+        }
+        animalRepository.save(animal);
         return animal;
     }
 
     public Integer createAnimal(Animal animal,Integer id) {
-        animal.setOwner(personService.getPerson(id));
-        animalList.getAnimalList().add(animal);
+        animal.setOwner(id);
+        animalRepository.save(animal);
         return animal.getId();
     }
 
     public void deleteAnimal(Integer id) {
-        Animal animal = animalList.getAnimalList().stream()
-                .filter(s->s.getId().equals(id))
-                .findAny()
-                .orElse(Animal.builder().build());
-        animalList.getAnimalList().remove(animal);
+        Animal animal = findAnimal(id);
+        animalRepository.delete(animal);
+    }
+
+    private Animal findAnimal(Integer id){
+        return animalRepository.findById(id).orElse(Animal.builder().build());
     }
 }
